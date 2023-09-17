54e4f1d73f21af778a6e873f9ff8a1af131b5ac6
victor <torvicv@gmail.com>
2023-09-17 10:24:43 +0200

    Simplified Accessors and Mutators

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
@@ -19,6 +20,12 @@ class Post extends Model
     protected $cast = [
         'state' => PostState::class
     ];
+
+    public function title(): Attribute {
+        return new Attribute(
+            set: fn($value) => strtoupper($value)
+        );
+    }
 
     public function toSearchableArray() {
         return [
